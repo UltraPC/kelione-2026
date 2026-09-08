76ae2fe2f610318fe3228e3f80644247062afc97
UltraPC <132989031+UltraPC@users.noreply.github.com>
2026-09-08 13:52:09 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kelione-2026-pwa-v2';
+const CACHE='kelione-2026-pwa-v3';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
